b945f2ed861754d09c18cb79c789efbfd66ce14a
Přemysl Vohnout <vohnout@ccss.cz>
2015-08-13 20:32:52 +0200

    sdi4apps update

--- themes/sdi4apps-theme/docroot/_diffs/js/iframefull.js
+++ themes/sdi4apps-theme/docroot/_diffs/js/iframefull.js
@@ -2,30 +2,48 @@ window.addEventListener ? window.addEventListener('load', fullSize, false) : win
 window.addEventListener ? window.addEventListener('resize', fullresize, false) : window.attachEvent('onresize', fullresize);
 
 function fullSize() {
-if ($('.iframefull').length > 0){
-	$('html').css('overflow','hidden');
-	var contDiv = $('iframe').parent();
-	contDiv.css('overflow','hidden');
-	contDiv.css('width','100%');
-	contDiv.css('height','200px');
-
-	contDiv.parent().css('overflow','hidden');
-	$('#content').resize(fullresize());
+    if ($('.iframefull').length > 0){
+        $('html').css('overflow','hidden');
+        $('.portlet-column-content').css('padding','0');
+        var iframe = $('iframe');
+        var contDiv = iframe.parent();
+        contDiv.css('overflow','hidden');
+        contDiv.css('width','100%');
+        contDiv.css('height','200px');
+        iframe.css('width','100%');
+        contDiv.parent().css('overflow','hidden');
+        $('#content').resize(fullresize());
+        fullresize();
+    }
 }
+
+function getHeight() {
+    var windowHeight = document.body.parentNode.clientHeight;
+    var headerHeight = 0;
+
+    var northDIV = ['.portlet-dockbar','#header'];
+
+    for (idiv = 0; idiv < northDIV.length; idiv++) {
+        if ($(northDIV[idiv]).length > 0) {
+            headerHeight += $(northDIV[idiv]).height();
+        }
+    }
+
+    var height = windowHeight - headerHeight;
+    return height;
 }
 
 function fullresize() {
-if ($('.iframefull').length > 0){
-	var iframe = $('iframe');
-	var contDiv = $('iframe').parent();
-	var width = $('#content').width();
-	var windowHeight = $(window).height();
-	var headerHeight = $('#banner').height() + 25;
-	var iframeHeight = windowHeight - headerHeight;
-	contDiv.height(iframeHeight);
-	contDiv.width(width);
-	iframe.attr('width',width);
-	iframe.attr('height',iframeHeight);
-}
-
+    if ($('.iframefull').length > 0){
+        var iframe = $('iframe');
+        var contDiv = $('iframe').parent();
+        var width = $('#content').width();
+        var iframeHeight = getHeight();
+        contDiv.height(iframeHeight);
+        iframe.height(iframeHeight);
+        contDiv.width(width);
+        iframe.width(width);
+        iframe.attr('width',width);
+        iframe.attr('height',iframeHeight);
+    }
 }
