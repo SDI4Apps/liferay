a197e375c70af0170cc241d568f7747e617c3d4f
Přemysl Vohnout <vohnout@ccss.cz>
2016-05-05 08:33:06 +0200

    MapViewers config update

--- portlets/mapViewer-lite-portlet/docroot/js/config-ng.js
+++ portlets/mapViewer-lite-portlet/docroot/js/config-ng.js
@@ -1,25 +1,18 @@
 'use strict';
 
 var hsl_path = '/wwwlibs/hslayers-ng/';
+var gitsha = $.ajax({
+    type: "GET",
+    url: hsl_path + 'gitsha.js',
+    async: false
+}).responseText;
+
 require.config({
+    urlArgs: 'bust=' + gitsha,
     paths: {
-        sidebar: hsl_path + 'components/sidebar/sidebar',
-        toolbar: hsl_path + 'components/toolbar/toolbar',
-        layermanager: hsl_path + 'components/layermanager/layermanager',
-        ows: hsl_path + 'components/ows/ows',
-        'ows.wms': hsl_path + 'components/ows/ows_wms',
-        'ows.nonwms': hsl_path + 'components/ows/ows_nonwms',
-        'ows.wmsprioritized': hsl_path + 'components/ows/ows_wmsprioritized',
-        query: hsl_path + 'components/query/query',
-        search: hsl_path + 'components/search/search',
-        permalink: hsl_path + 'components/permalink/permalink',
-        geolocation: hsl_path + 'components/geolocation/geolocation',
-        measure: hsl_path + 'components/measure/measure',
-        legend: hsl_path + 'components/legend/legend',
+        ol: hsl_path + 'node_modules/openlayers/dist/ol-debug',
         app: '/js/map/config-lite',
-        core: hsl_path + 'components/core/core',
-        api: hsl_path + 'components/api/api',
-        translations: hsl_path + 'components/translations/js/translations'
+        core: hsl_path + 'components/core/core'
     },
     shim: {
         d3: {
